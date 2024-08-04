68ca14537d11fc2ed3a98c9ef67e59cc860a21e4
Imran Khan <imran.4.khan@gm.com>
2024-08-04 19:07:42 +0530

    Selenium Ecommerce - E2E Flow

--- src/test/java/Tests/ErrorValidations.java
+++ src/test/java/Tests/ErrorValidations.java
@@ -1,13 +1,26 @@
 package Tests;
 
+import PageObjects.CartPage;
+import PageObjects.ProductCatalogue;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ErrorValidations extends BaseTest{
 
     @Test
-    public void errorValidation(){
+    public void loginErrorValidation(){
         landingPage.login("invalidUsername@yopmail.com","Test1234");
         Assert.assertEquals(landingPage.getErrorMessage(), "Incorrect email or password.");
+    }
+
+    @Test
+    public void productErrorValidation(){
+        String productName="ZARA COAT 3";
+
+        ProductCatalogue productCatalogue=landingPage.login("yobeje7646@eixdeal.com","Test@1234");
+        productCatalogue.addProduct(productName);
+
+        CartPage cartPage=productCatalogue.goToCart();
+        Assert.assertFalse(cartPage.verifyProductinCart("iPhone"));
     }
 }
